e416efb56835d41c1c601e60c1b2098836d9e0c1
Mukhammed <massalimov.mukhammed@mail.ru>
2024-09-01 18:09:50 +0500

    Modification 3

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { isMobile } from "../functions";
 import Checkbox from "./Checkbox";
 import Action from "./Action";
